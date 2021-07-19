d16c44a79b5b0c74e0ef72bf982fcd586f23e04e
Christian Segota <Christian.Segota@gmail.com>
2021-07-19 14:59:22 -0700

    Basic Layout Structure + Headers HTML + package-lock
    Header, Content, and Footer place holders. Two example header implementations.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./styles.css";
 
+import Header from "./master/Header"
+import MainContent from "./master/MainContent"
+import Footer from "./master/Footer"
+
 export default function App() {
   return (
     <div className="App">
-      <h1>DL-edu</h1>
-      <h2>Welcome to the project</h2>
-      <h2>GG WP</h2>
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
---
+++ src/master/Header.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+import './Header.css';
+
+function Header() {
+    return (
+        //HEADER 1, RESPONSIVE - https://www.w3schools.com/howto/howto_css_responsive_header.asp
+        // <div>
+        //     <div class="header">
+        //         <a href="#default" class="logo">Dev Launchers: Base Camp</a>
+        //         <div class="header-right">
+        //             <a class="active" href="#home">Home</a>
+        //             <a href="#contact">Contact</a>
+        //             <a href="#about">About</a>
+        //         </div>
+        //     </div>
+        // </div>
+
+        //HEADER 2, RESPONSIVE TOPNAV - https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
+        <div>
+            <div class="topnav" id="myTopnav">
+            <a href="#home" class="active">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                <i class="fa fa-bars"></i>
+            </a>
+            </div>
+        </div>
+    )
+}
+
+export default Header
