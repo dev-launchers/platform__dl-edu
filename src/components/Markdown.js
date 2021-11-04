5688f5e6c86097471283efdc5bb2974ca2ad6179
Jack Daus <jackdaus@gmail.com>
2021-11-03 23:17:11 -0400

    Ability to edit markdown using stackedit.js

---
+++ src/components/Markdown.js
@@ -0,0 +1,49 @@
+import Stackedit from "stackedit-js"
+import ReactMarkdown from "react-markdown"
+import rehypeHighlight from "rehype-highlight"
+import { useState } from "react"
+
+import Fab from "@material-ui/core/Fab"
+import EditIcon from "@material-ui/icons/Edit"
+
+import "highlight.js/styles/base16/zenburn.css"
+
+function Markdown(props) {
+    const [markdown, setMarkdown] = useState(props.markdown);
+    
+    const openStackedit = () => {
+        const stackedit = new Stackedit()
+        stackedit.openFile({
+            name: "filename",
+            content: {
+                text: markdown
+            }
+        })
+
+        stackedit.on("fileChange", (file) => {
+            setMarkdown(file.content.text)
+        })
+
+        stackedit.on("close", () => {
+            // TODO when user closes modal, post to backend to save changes
+        })
+    }
+
+    return (
+        <div>
+            {props.canEdit &&
+                <Fab color="primary" style={{float: "right", right: "10px"}} onClick={openStackedit}>
+                    <EditIcon/>
+                </Fab>
+            }
+            <div>
+                <ReactMarkdown
+                    children={markdown}
+                    rehypePlugins={[rehypeHighlight]}>
+                </ReactMarkdown>
+            </div>
+        </div>
+    )
+}
+
+export default Markdown
