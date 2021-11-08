4fb2d242f95da1afb3e66d93861ca64ef1d7b0e7
Perry <80589286+pcrain9@users.noreply.github.com>
2021-11-08 14:56:27 -0600

    rough draft complete.  still need icon images.

--- src/master/DarkFooter.js
+++ src/master/DarkFooter.js
@@ -8,6 +8,7 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
+import classes from "./DarkFooter.module.css";
 //override default mui styling
 const cache = createCache({
   key: "css",
@@ -16,13 +17,39 @@ const cache = createCache({
 function DarkFooter() {
   return (
     <CacheProvider value={cache}>
-      <Box sx={{backgroundColor:'#222222'}}>
-        <Container>
-          <Grid>
-            <Typography variant="h2">Sign up for our newsletter!</Typography>
-          </Grid>
-        </Container>
-      </Box>
+      <Grid
+        container
+        columns={5}
+        spacing={2}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        className={classes.footerContainer}
+      >
+        <Grid item xs={12}>
+          <Typography variant="h2" sx={{ color: "#ffffff" }}>
+            Sign up for our newsletter!
+          </Typography>
+        </Grid>
+        <Grid item className={classes.emailQueryContainer}>
+          <TextField size='small' placeholder="Enter your email" className={classes.emailQueryField}/>
+          <Button className={classes.emailQueryButton}>Submit</Button>{" "}
+        </Grid>
+        <Grid item>
+          <img />
+          <img />
+          <img />
+          <img />
+          <img />
+        </Grid>
+        <Grid item>
+          <Typography variant="h5" sx={{ color: "#ffffff", margin:'auto',width:'80%', textAlign:'center' }}>
+            “Tell me and I forget, teach me and I may remember, involve me and I
+            learn” - Benjamin Franklin
+          </Typography>
+        </Grid><Grid item xs />
+        <Grid item><Typography paragraph={true} sx={{ color: "#ffffff" }}> ©Dev Launchers, 2021</Typography></Grid>
+      </Grid>
     </CacheProvider>
   );
 }
