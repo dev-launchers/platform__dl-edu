69a087ebcaeb12815fd6fa4c2a8fd5407b3f6609
Jack Daus <jackdaus@gmail.com>
2021-11-13 21:16:20 -0500

    MUI update: add theme provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./styles.css";
 
+
 import Header from "./master/Header"
 import MainContent from "./master/MainContent"
 
+import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core/styles';
+
+const theme = createMuiTheme();
+
 export default function App() {
   return (
-    <div className="App">
-      <Header />
-      <MainContent />
-    </div>
+    <ThemeProvider theme={theme}>
+        <div className="App">
+            <Header />
+            <MainContent />
+        </div>
+    </ThemeProvider>
+    
   );
 }
