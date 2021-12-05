bf058e4c4300895379e16fc50e2c43770b83335d
Perry <80589286+pcrain9@users.noreply.github.com>
2021-12-05 11:41:58 -0600

    not ready to commit this yet

--- src/main/DarkFooter.js
+++ src/main/DarkFooter.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { CacheProvider } from "@emotion/react";
+import { styled } from "@mui/system";
 import createCache from "@emotion/cache";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
-import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
@@ -21,12 +21,21 @@ const cache = createCache({
   key: "css",
   prepend: true,
 });
+
+const IconLink = styled("Link")(({ theme }) => ({
+  backgroundColor:"#222222",
+  color:"#ffffff",
+  heigh:"20px",
+  width:"20px",
+  borderRadius:"50%"
+}));
+
 function DarkFooter() {
   return (
     <CacheProvider value={cache}>
       <Box className={classes.footerContainer}>
         <Container sx={{ width: "20%" }} className={classes.leftContainer}>
-          <Stack direction="column">
+          <Stack direction="column" spacing={3}>
             <Box
               display="flex"
               width="65%"
@@ -42,91 +51,112 @@ function DarkFooter() {
                 DL Basecamp
               </Typography>
             </Box>
-            <Typography color="#ffffff" paragraph>
-              Copyright © 2021 DevLauchers.
-            </Typography>
-            <Typography color="#ffffff" paragraph>
-              All rights reserved
-            </Typography>
+            <Box>
+              <Typography color="#ffffff" paragraph>
+                Copyright © 2021 DevLauchers.
+              </Typography>
+              <Typography color="#ffffff" paragraph>
+                All rights reserved
+              </Typography>
+            </Box>
             <Box className={classes.imagesContainer}>
-              <Card raised backgroundColor="#222222">
-                <Link href="http://www.instagram.com">
-                  <img src={instagramIcon} className={classes.footerImage} />
-                </Link>
-              </Card>
-              <Card raised backgroundColor="#222222">
+                <IconLink href="http://www.instagram.com">
+                  <img
+                    src={instagramIcon}
+                    alt="null"
+                    className={classes.footerImage}
+                  />
+                </IconLink>
+              {/* <IconCard raised>
                 <Link href="http://www.linkedin.com">
-                  <img src={linkedinIcon} className={classes.footerImage} />
+                  <img
+                    src={linkedinIcon}
+                    alt="null"
+                    className={classes.footerImage}
+                  />
                 </Link>
-              </Card>
-              <Card raised backgroundColor="#222222">
-                <Link href="http://www.devlaunchers.com"></Link>
-              </Card>
-              <Card>
-                <Link href="http://www.twitch.com">
-                  <img src={twitchIcon} className={classes.footerImage} />
+              </IconCard>
+              <IconCard raised>
+                <Link
+                  href="http://www.twitch.com"
+                  sx={{ backgroundColor: "#222222" }}
+                >
+                  <img
+                    src={twitchIcon}
+                    alt="null"
+                    className={classes.footerImage}
+                  />
                 </Link>
-              </Card>
-              <Card raised backgroundColor="#222222">
+              </IconCard>
+              <IconCard raised >
                 <Link href="http://www.discord.com">
-                  <img src={discordIcon} className={classes.footerImage} />
+                  <img
+                    src={discordIcon}
+                    alt="null"
+                    className={classes.footerImage}
+                  />
                 </Link>
-              </Card>
+              </IconCard> */}
             </Box>
           </Stack>
         </Container>
         <Box className={classes.rightContainer}>
-          <Stack direction="column" width="15%" spacing={3}>
+          <Stack direction="column" width="15%" spacing={2}>
             <Typography color="#ffffff" variant="h6">
               Company
             </Typography>
-            <Link to="">
-              <Typography color="#ffffff" paragraph>
-                About us
-              </Typography>
-            </Link>
-            <Link to="">
-              <Typography color="#ffffff" paragraph>
-                Contact us
-              </Typography>
-            </Link>
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <Link underline="none" href="http://www.espn.com">
+                <Typography color="#ffffff" paragraph>
+                  About us
+                </Typography>
+              </Link>
+              <Link underline="none" href="">
+                <Typography color="#ffffff" paragraph>
+                  Contact us
+                </Typography>
+              </Link>
+            </Box>
           </Stack>
-          <Stack direction="column" width="15%">
+          <Stack direction="column" width="15%" spacing={2}>
             <Typography color="#ffffff" variant="h6">
               Support
             </Typography>
-            <Link to="">
-              <Typography color="#ffffff" paragraph>
-                Terms of service
-              </Typography>
-            </Link>
-            <Link to="">
-              <Typography color="#ffffff" paragraph>
-                Privacy policy
-              </Typography>
-            </Link>
-            <Link to="">
-              <Typography color="#ffffff" paragraph>
-                Support us
-              </Typography>
-            </Link>
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <Link underline="none" href="">
+                <Typography color="#ffffff" paragraph>
+                  Terms of service
+                </Typography>
+              </Link>
+              <Link underline="none" href="">
+                <Typography color="#ffffff" paragraph>
+                  Privacy policy
+                </Typography>
+              </Link>
+              <Link underline="none" href="">
+                <Typography color="#ffffff" paragraph>
+                  Support us
+                </Typography>
+              </Link>
+            </Box>
           </Stack>
-          <Stack direction="column" width="30%">
+          <Stack direction="column" width="30%" spacing={2}>
             <Typography variant="h6" sx={{ color: "#ffffff" }}>
               Sign up for our newsletter!
             </Typography>
-            <TextField
-              size="small"
-              placeholder="Enter your email"
-              className={classes.emailQueryField}
-            />
-            <Button
-              className={classes.emailQueryButton}
-              variant="contained"
-              color="secondary"
-            >
-              Submit
-            </Button>{" "}
+            <Box display="flex">
+              <input
+                placeholder="Enter your email"
+                className={classes.emailQueryField}
+              />
+              <Button
+                className={classes.emailQueryButton}
+                variant="contained"
+                color="secondary"
+              >
+                Submit
+              </Button>
+            </Box>
           </Stack>
         </Box>
       </Box>
